ccd05943c350fc67452591d9b6cf04e75d2334b1
Syed Ishmum Ahnaf <syedishmum15@gmail.com>
2025-01-31 04:20:23 +0600

    Modified PyrDown parameter intake to deal with version issue

--- src/TorchFovea.py
+++ src/TorchFovea.py
@@ -41,7 +41,8 @@ class TorchFovea(torch.nn.Module):
         # create kernel pyramid
         kernels = [self.createFilter(m, n)]
         for i in range(self.level):
-            kernel_down = PyrDown()(kernels[i].unsqueeze(0).unsqueeze(0)).squeeze(0).squeeze(0)
+            kernel_down = PyrDown()(kernels[i].unsqueeze(0).unsqueeze(0))  # ✅ Fix: Ensure `()` is called correctly
+            kernel_down = kernel_down.squeeze(0).squeeze(0)
             kernels.append(kernel_down)
         return kernels
 
