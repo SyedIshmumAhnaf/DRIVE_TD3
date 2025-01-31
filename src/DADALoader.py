d7d9e9c38115160706510ca246e81e9e48009607
Syed Ishmum Ahnaf <syedishmum15@gmail.com>
2025-01-31 09:05:31 +0600

    modifying custom collate function to take three parameters only

--- src/DADALoader.py
+++ src/DADALoader.py
@@ -303,8 +303,8 @@ class PreFetcher():
         return next_video_data, next_focus_data, next_coord_data
 
 def custom_collate_fn(batch):
-    print("Batch structure:", batch)
-    video_data, coord_data, data_info = zip(*batch)
+    #print("Batch structure:", batch)
+    video_data, coord_data, data_info, *_ = zip(*batch)
     return (
         torch.stack([torch.as_tensor(v) for v in video_data]), 
         torch.stack([torch.as_tensor(c) for c in coord_data]), 
